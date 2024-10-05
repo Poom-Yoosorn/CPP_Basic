8e1a6891876d2700f7b7ac5771b56676a5045b62
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-05 22:51:24 +0700

    While loop

--- basics_level_3/main.cpp
+++ basics_level_3/main.cpp
@@ -15,9 +15,16 @@ int main()
     }
 
 
-    for (int i = 0; i < 10;i++){
+    // for (int i = 0; i < 10;i++){
+    //     cout << "Hello" << i << endl;
+    // }
+
+    int i = 0;
+    while(i < 10){
         cout << "Hello" << i << endl;
+        i++;
     }
+
 
     return 0;
 }
