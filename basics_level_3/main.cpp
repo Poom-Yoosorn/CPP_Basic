98b2d3c91d3f5c117eddb259b4a5f9a7b03e6a42
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-05 12:15:05 +0700

    Condition Statements with Booleans

---
+++ basics_level_3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << boolalpha;
+    cout << (1 == 1) << endl;
+    cout << (2 != 1) << endl;
+    cout << (2 < 1) << endl;
+    cout << (2 > 1) << endl;
+    cout << (1 <= 1) << endl;
+    cout << (1.01 >= 1) << endl;
+    cout << ("hello" == "HELLO") << endl;
+    return 0;
+}
