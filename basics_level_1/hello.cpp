cb6cb83098bb43f53980d7ccbd4ffbbe65c67aee
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-05 08:18:27 +0700

    Variable - Constants

--- basics_level_1/hello.cpp
+++ basics_level_1/hello.cpp
@@ -4,6 +4,9 @@ int a;
 
 int main()
 {
+    const int secounds_per_hour = 3600;
+    const double max_allowed_temperature = 76.8;
+
     int user_age = 2;
     double temp = 20.6;
     bool is_alive = true;
@@ -13,6 +16,8 @@ int main()
     std::cout << temp << std::endl;
     std::cout << is_alive << std::endl;
     std::cout << user_name << std::endl;
+    std::cout << secounds_per_hour << std::endl;
+    std::cout << max_allowed_temperature << std::endl;
     
     return 0;
 }
