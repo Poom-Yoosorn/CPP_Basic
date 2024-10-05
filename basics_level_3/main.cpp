3225fbb482b4952e7d940dbc79fb8eadf2a1ef1d
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-05 12:22:22 +0700

    Conbining Conditional Statements

--- basics_level_3/main.cpp
+++ basics_level_3/main.cpp
@@ -10,5 +10,15 @@ int main()
     cout << (1 <= 1) << endl;
     cout << (1.01 >= 1) << endl;
     cout << ("hello" == "HELLO") << endl;
+
+
+    cout << ((1.01 >= 1) || (2 > 3)) << endl; //or
+    cout << ((1.01 >= 1) && (4 > 3)) << endl; //and
+
+
+    double temp = 2.0;
+    cout << ((temp >= 1) && (temp < 3)) << endl; //and
+
+
     return 0;
 }
