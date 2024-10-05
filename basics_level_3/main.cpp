7447c5f5aa5356af3ecbde9b05514c13e47e614e
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-05 23:09:06 +0700

    Loop And Array

--- basics_level_3/main.cpp
+++ basics_level_3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
@@ -19,12 +20,35 @@ int main()
     //     cout << "Hello" << i << endl;
     // }
 
-    int i = 0;
-    while(i < 10){
-        cout << "Hello" << i << endl;
-        i++;
+    // int i = 0;
+    // while(i < 10){
+    //     cout << "Hello" << i << endl;
+    //     i++;
+    // }
+
+//--------------------------------------------------------------------
+    vector<double> temp_list = {-12.2,-14.6,15.5,78.3};
+    for (int i = 0; i < temp_list.size(); i++){
+        double temp = temp_list.at(i);
+        cout << temp;
+        if (temp < 0){
+            cout << " lower than zero" << endl;
+        }
+        else{
+            cout << endl;
+        }
     }
 
+    for ( double temp: temp_list){
+        cout << temp << endl;
+    }
 
+//--------------------------------------------------------------------
+
+    double temps[4] = {12.2,14.6,15.5,78.3};
+    for (int i = 0; i < 4; i++){
+        cout << temps[i] << endl;
+    }
+//--------------------------------------------------------------------
     return 0;
 }
