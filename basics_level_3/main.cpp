b35fc5e27973c45313155e0a6ebdbccefb98f434
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-05 22:32:05 +0700

    If,Else

--- basics_level_3/main.cpp
+++ basics_level_3/main.cpp
@@ -2,23 +2,18 @@
 using namespace std;
 int main()
 {
-    cout << boolalpha;
-    cout << (1 == 1) << endl;
-    cout << (2 != 1) << endl;
-    cout << (2 < 1) << endl;
-    cout << (2 > 1) << endl;
-    cout << (1 <= 1) << endl;
-    cout << (1.01 >= 1) << endl;
-    cout << ("hello" == "HELLO") << endl;
+    int user_age = 70;
 
+    if (user_age <= 18) {
+        cout << "You are officially an Child." << endl;
+    }
+    else if((user_age > 18) && (user_age < 27)){
+        cout << "You are officially an Adult below 27." << endl;
+    }
+    else{
+        cout << "You are officially an Adult Old." << endl;
+    }
 
-    cout << ((1.01 >= 1) || (2 > 3)) << endl; //or
-    cout << ((1.01 >= 1) && (4 > 3)) << endl; //and
-
-
-    double temp = 2.0;
-    cout << ((temp >= 1) && (temp < 3)) << endl; //and
-
-
+    cout << "End of program." << endl;
     return 0;
 }
