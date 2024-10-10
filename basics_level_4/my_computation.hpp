259b72a95ebd5daf2970bb82c00c7caf82ec4332
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-10 18:20:16 +0700

    Organize your code with .hpp file

---
+++ basics_level_4/my_computation.hpp
@@ -0,0 +1,15 @@
+
+
+#ifndef MY_COMPUTATION_H
+#define MY_COMPUTATION_H
+
+#include <iostream>
+using namespace std;
+int triple_number(int number);
+void print_triple_number(int number);
+
+
+
+
+
+#endif
---
+++ basics_level_4/prototype.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "my_computation.hpp"
+using namespace std;
+
+int main()
+{
+    print_triple_number(44);
+    return 0;
+}
+
+
